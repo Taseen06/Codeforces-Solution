4668e1bd6a5262fd0d276cb643ec28b890d0c0fb
Toufiqur Rahman Tasin <135156465+Taseen06@users.noreply.github.com>
2024-11-20 20:28:58 +0600

    .

---
+++ 1968 - A. Maximize.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    cout<<n-1<<endl;
+}
+
+int main()
+{
+    
+    int TCS = 1;
+    cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+      
+      solve();
+    }
+}
