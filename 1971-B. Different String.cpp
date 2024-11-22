83166762c7f2d0c10184fb80198527747cd98121
Toufiqur Rahman Tasin <135156465+Taseen06@users.noreply.github.com>
2024-11-22 21:52:17 +0600

    .

---
+++ 1971-B. Different String.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve()
+{
+    string s;
+    cin>>s;
+    bool ok=0;
+    for(int i=1; i<s.size(); ++i)
+    {
+        if(s[i]!=s[0])
+        {
+            swap(s[i],s[0]);
+            ok=1;
+            break;
+        }
+    }
+    if(ok) cout<<"YES\n"<<s<<endl;
+    else cout<<"NO\n";
+}
+ 
+signed main()
+{
+   
+    int TCS = 1;
+    cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        
+        solve();
+    }
+}
